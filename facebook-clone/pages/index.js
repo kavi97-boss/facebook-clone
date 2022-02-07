38d8188d6876c4319fe7e6fb9e6f45948e1e6001
kavi99 <kavindu.harshitha97@gmail.com>
2022-02-07 23:01:35 +0530

    renamed project

---
+++ facebook-clone/pages/index.js
@@ -0,0 +1,22 @@
+import '../styles/Home.module.css';
+
+export default function Home() {
+    return (
+        <div className="container">
+            <div className="inner-home">
+                <div className="left">
+                    <div className="inner">
+                        <h1>mybook</h1>
+                        <h3>
+                            mybook helps you connect and share with the people
+                            in your life.
+                        </h3>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="inner-win"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
